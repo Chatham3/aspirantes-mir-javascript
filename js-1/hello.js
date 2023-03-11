ef34d09e6f762713a171c7fe2e5e2f44b6aa2ea8
Sebastián <montoyafdez@hotmail.com>
2023-03-10 21:18:40 -0500

    Solución a los ejercicios de js

---
+++ js-1/hello.js
@@ -0,0 +1,8 @@
+// escribe la función hola acá
+function hola (nombre) {
+    return "Hola " + nombre + "!"
+}
+// código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("Sebastián")) // "Hola !"
