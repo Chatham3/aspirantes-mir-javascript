f089b636561dc9747f3db55f7ae471cdbdbb0f90
Sebastián <montoyafdez@hotmail.com>
2023-03-13 19:33:49 -0500

    Solución ejercicios de js

---
+++ js-2/maxindex.js
@@ -0,0 +1,18 @@
+function maxIndex (arr) {
+    if (arr.length === 0) {
+        return -1;
+        }
+    
+    let maxIndex = 0;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr [1]> arr [maxIndex]) {
+            maxIndex=1;
+        }
+    
+    }
+    return maxIndex;
+}
+
+console.log(maxIndex([1, 3, 2])) // 1
+console.log(maxIndex([10, 9, 8, 7, 6, 5, 4])) // 0
+console.log(maxIndex([])) // -1
